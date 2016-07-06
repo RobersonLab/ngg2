6276026afe622fb7f6e4649b402ae1e0682d5235
Eli Roberson <eroberso@dom.wustl.edu>
2016-07-06 13:43:15 -0500

    Fixed random errors in test functions

--- ngg2/tests/test_multiproc_ngg_scan.py
+++ ngg2/tests/test_multiproc_ngg_scan.py
@@ -1,14 +1,8 @@
 import os
 
-TEST_FILE = os.path.join( os.path.dirname( __file__ ), 'test.fa' )
+TEST = os.path.join( os.path.dirname( __file__ ), 'test.fa' )
 
 from ngg2 import rev_comp, compile_regex_patterns, tuple_to_key, Grna, multiproc_ngg_scan
-
-#def multiproc_ngg_scan( fasta_fname, region_tuple, regex_tuple, scan_type, max_g_content ):
-# contig, start, end, strand
-# def __init__( self, contig, positionStart, strand, regexMatch, inMatch=None, inStart=None )
-GAGAAGACTATTTCCGTAGGAGG a plus
-AAGACTATTTCCGTAGGAGGTGG b plus
 
 def test_ngg_scan_plus_block():
 	strand = '+'
@@ -19,7 +13,9 @@ def test_ngg_scan_plus_block():
 	
 	out = multiproc_ngg_scan( TEST, ( contig, start_pos, 240, strand ), compile_regex_patterns( scantype, allow_non_g_start ), scantype, 15 )
 	
-	assert( out == [ Grna( contig, start_pos, strand, None, 'GAGAAGACTATTTCCGTAGGAGG', 1 ) ] )
+	test = [ Grna( contig, start_pos, strand, None, 'GAGAAGACTATTTCCGTAGGAGG', 0 ) ]
+	
+	assert( out[1] == test )
 	
 def test_ngg_scan_plus_exhaustive():
 	strand = '+'
@@ -30,7 +26,9 @@ def test_ngg_scan_plus_exhaustive():
 	
 	out = multiproc_ngg_scan( TEST, ( contig, start_pos, 240, strand ), compile_regex_patterns( scantype, allow_non_g_start ), scantype, 15 )
 	
-	assert( out == [ Grna( contig, start_pos, strand, None, 'GAGAAGACTATTTCCGTAGGAGG', 0 ), Grna( contig, start_pos, strand, None, 'AAGACTATTTCCGTAGGAGGTGG', 3 ) ] )
+	test = [ Grna( contig, start_pos, strand, None, 'GAGAAGACTATTTCCGTAGGAGG', 0 ), Grna( contig, start_pos, strand, None, 'AAGACTATTTCCGTAGGAGGTGG', 3 ) ]
+	
+	assert( out[1] == test )
 	
 def test_ngg_scan_minus_block():
 	strand = '-'
@@ -41,7 +39,9 @@ def test_ngg_scan_minus_block():
 	
 	out = multiproc_ngg_scan( TEST, ( contig, start_pos, 240, strand ), compile_regex_patterns( scantype, allow_non_g_start ), scantype, 15 )
 	
-	assert( out == [ Grna( contig, start_pos, strand, None, 'CCTCCTACGGAAATAGTCTTCTC', 216 ) ] )
+	test = [ Grna( contig, start_pos, strand, None, 'CCTCCTACGGAAATAGTCTTCTC', 217 ) ]
+	
+	assert( out[1] == test )
 
 def test_ngg_scan_minus_exhaustive():
 	strand = '-'
@@ -52,4 +52,6 @@ def test_ngg_scan_minus_exhaustive():
 	
 	out = multiproc_ngg_scan( TEST, ( contig, start_pos, 240, strand ), compile_regex_patterns( scantype, allow_non_g_start ), scantype, 15 )
 	
-	assert( out == [ Grna( contig, start_pos, strand, None, 'CCTCCTACGGAAATAGTCTTCTC', 216 ) ] )
+	test = [ Grna( contig, start_pos, strand, None, 'CCTCCTACGGAAATAGTCTTCTC', 217 ) ]
+	
+	assert( out[1] == test )
